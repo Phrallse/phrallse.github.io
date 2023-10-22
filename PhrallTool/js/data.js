1cb705a2b0222dd4be76413561179fae1b8c4ab3
Phrallse-平行语 <88480640+Phrallse@users.noreply.github.com>
2023-10-22 21:42:24 +0800

    Add files via upload

---
+++ PhrallTool/js/data.js
@@ -0,0 +1,7 @@
+const data = [{
+		"name": "Momotalk  Editor",
+		"img": "https://api.kivo.wiki/assets/images/gallery/16/ClanChat_Emoji_102_Jp.png",
+		"info": "蔚蓝档案中Momotalk的编辑器",
+		"href": "./momotalk"
+	}
+];
